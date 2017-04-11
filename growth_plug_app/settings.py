62fed591aa214873c7ae86c347a43737951be551
divyum <divyumrastogi@gmail.com>
2017-04-11 12:14:12 +0530

    small fix

--- growth_plug_app/settings.py
+++ growth_plug_app/settings.py
@@ -16,7 +16,6 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 AUTH_USER_EMAIL_UNIQUE = True
-DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -112,6 +111,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
